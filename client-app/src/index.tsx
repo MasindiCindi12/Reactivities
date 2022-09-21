49631ed79789e2baf4c6e1ca15450e4f5c8df2be
Masindi Cindy <59694595+MasindiCindi12@users.noreply.github.com>
2022-09-21 12:12:13 +0200

    End Of Section 5

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -11,6 +11,7 @@ root.render(
  
     <App />,
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
